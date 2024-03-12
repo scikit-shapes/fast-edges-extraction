0236a615d0bdc0177098b408520638a231bde371
Louis-Pujol <louispujol@laposte.net>
2024-03-12 13:29:27 +0100

    write docstring for extact_edges

--- fast_edge_extraction/__init__.py
+++ fast_edge_extraction/__init__.py
@@ -10,7 +10,48 @@ def extract_edges(
     return_adjacency: bool = False,
 
 ) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]]:
-    """Interface to the cython function edges_cython_core"""
+    """Extract the edges from a set of triangles.
+
+    With default parameters, this function returns the edges of the triangles
+    as a 2D array of shape (n_edges, 2), where each row contains the indices of
+    the two points that define the edge.
+
+    If `return_adjacency` is set to `True`, the function also returns the
+    degrees of each edge, and the indices of the triangles and points that are
+    adjacent to each edge (limited to 2 triangles and 2 points per edge).
+
+    Parameters
+    ----------
+    triangles : np.ndarray
+        A 2D array of shape (n_triangles, 3) containing the indices of the
+        points that define each triangle.
+
+    return_adjacency : bool, optional
+        If `True`, the function also returns the degrees of each edge, and the
+        indices of the triangles and points that are adjacent to each edge
+        (limited to 2 triangles and 2 points per edge). Default is `False`.
+
+    Returns
+    -------
+    edges : np.ndarray
+        A 2D array of shape (n_edges, 2) containing the indices of the points
+        that define each edge.
+
+    degrees : np.ndarray
+        A 1D array of shape (n_edges,) containing the degree of each edge. This
+        is the number of triangles that share the edge. Only returned if
+        `return_adjacency` is `True`.
+
+    adjacent_triangles : np.ndarray
+        A 2D array of shape (n_edges, 2) containing the indices of the triangles
+        that are adjacent to each edge. Only returned if `return_adjacency` is
+        `True`.
+
+    adjacent_points : np.ndarray
+        A 2D array of shape (n_edges, 2) containing the indices of the points
+        that are adjacent to each edge. Only returned if `return_adjacency` is
+        `True`.
+    """
 
     if triangles.shape[1] != 3:
         msg = "Triangles should have shape (n_triangles, 3)"
